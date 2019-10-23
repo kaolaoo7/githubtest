ddc4f8b39816b58050b9f81aa76deb6d4e11691f
Cny0306 <53330161+Cny0306@users.noreply.github.com>
2019-10-23 20:31:45 +0800

    Update targetyt.java

--- src/main/java/com/qf/targetyt.java
+++ src/main/java/com/qf/targetyt.java
@@ -7,7 +7,5 @@ package com.qf;
 public class targetyt {
     public static void main(String[] args) {
         System.out.println("测试一下..");
-        System.out.println("测试一下下哦哦哦..");
-        System.out.println("dev add test...好");
     }
 }
