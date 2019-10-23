ba4e1455be84cb32c1dc2ef37f73514d268042ea
yangting <406763876@qq.com>
2019-10-23 19:51:37 +0800

    Initial commit

---
+++ src/main/java/com/qf/targetyt.java
@@ -0,0 +1,11 @@
+package com.qf;
+
+/**
+ * @Auther YangTing
+ * @Date 2019/10/23
+ */
+public class targetyt {
+    public static void main(String[] args) {
+        System.out.println("测试一下..");
+    }
+}
