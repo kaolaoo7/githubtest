3ef4cf50ea8add50a5fcefbb937f5a7922d8762f
kaolaoo7 <53507196+kaolaoo7@users.noreply.github.com>
2019-10-23 20:10:25 +0800

    Update targetyt.java

--- src/main/java/com/qf/targetyt.java
+++ src/main/java/com/qf/targetyt.java
@@ -7,6 +7,6 @@ package com.qf;
 public class targetyt {
     public static void main(String[] args) {
         System.out.println("测试一下..");
-        System.out.println("测试一下下..");
+        System.out.println("测试一下下哦哦哦..");
     }
 }
