a54ba4e1ecdb56d1f7388907876350e7b78d14fe
yangting <406763876@qq.com>
2019-10-23 20:20:56 +0800

    dev add

--- src/main/java/com/qf/targetyt.java
+++ src/main/java/com/qf/targetyt.java
@@ -8,5 +8,6 @@ public class targetyt {
     public static void main(String[] args) {
         System.out.println("测试一下..");
         System.out.println("测试一下下哦哦哦..");
+        System.out.println("dev add test...");
     }
 }
